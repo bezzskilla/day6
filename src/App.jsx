5bcc9cbea6f60945da772a095ab5571d209c8d36
Безобразов Григорий <gabezobrazov@sbercloud.ru>
2023-03-03 21:43:47 +0300

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer'
+import Main from './components/Main';
+import { useState } from 'react';
+
+//компонент ререндерится при изменении внутреннего состояния
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementClick = () => {
+    // setCount(newState)
+    setCount((prev) => prev + 1)
+  }
+
+  console.log('render');
+
+  // let exCount = 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <hr />
+      {count}
+      <hr />
+
+      <button onClick={incrementClick} >Plus one</button>
+      <button onClick={() => setCount((prev) => prev - 1)} >Minus one</button>
+
+      <hr />
+
+      {/* {exCount}
+      BAD PRACTICE
+      <button onClick={() => exCount++} >Plus one</button> */}
+
+      <Main />
+
+      <Footer />
     </div>
   );
 }
